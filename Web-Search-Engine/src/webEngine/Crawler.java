ad05190910b1c3abf0b64ed595ce527917e0251a
nishantu1090 <upadhyan@uwindsor.ca>
2021-11-26 21:21:54 -0500

    introducting multithreading

--- Web-Search-Engine/src/webEngine/Crawler.java
+++ Web-Search-Engine/src/webEngine/Crawler.java
@@ -1,5 +1,7 @@
 package webEngine;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -10,12 +12,16 @@ import org.jsoup.select.Elements;
 public class Crawler implements Runnable {
 
 	private static Set<String> urls = new HashSet<String>();
-	private static int maxDepth = 5;
+	private static int maxDepth = 25;
 	private static String regex = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)";
-	private static String lastCrawledUrl;
-	
+	public static String lastCrawledUrl;
+	private static String[] webUrlsToCrawl = new String[5];
+	public static Instant startTime;
 	static {
-		lastCrawledUrl = "http://www.javatpoint.com";
+		
+		startTime = Instant.now();
+		webUrlsToCrawl = getArrayOfURLs();
+		lastCrawledUrl = webUrlsToCrawl[3];
 	}
 	public static void startCrawling(String url, int depth) {
 		Pattern patternObject = Pattern.compile(regex);
@@ -27,19 +33,33 @@ public class Crawler implements Runnable {
 					urls.add(url);
 					
 					Elements linksInWebPage = doc.select("a[href]");
-					
-					for(Element link : linksInWebPage) {
-						
-						if (isValidUrl(link.attr("abs:href")) && patternObject.matcher(link.attr("href")).find()) {
+					if(linksInWebPage.size() != 0)
+						for(Element link : linksInWebPage) {
 							
-							if(!isSameDomain(link.attr("abs:href"), url))
-								startCrawling(link.attr("abs:href"), ++depth);
-							else
-								continue;
+							if (isValidUrl(link.attr("abs:href")) && patternObject.matcher(link.attr("href")).find()) {
+								
+								if(!isSameDomain(link.attr("abs:href"), url))
+								{
+									float timeElapsed = Duration.between(startTime, Instant.now()).toMillis();
+									if(timeElapsed > 30000)
+										return;
+									else
+										startCrawling(link.attr("abs:href"), ++depth);
+								}
+								else
+									continue;
+							}
 						}
+					else
+					{
+						Random r = new Random();
+						int i = r.nextInt(webUrlsToCrawl.length);
+						startCrawling(webUrlsToCrawl[i], ++ depth);
+						
 					}
+						
 				}
-			} catch (IOException e) {
+			} catch (IOException e) { 
 				// TODO Auto-generated catch block
 				
 			}
@@ -80,7 +100,7 @@ public class Crawler implements Runnable {
 		
 		
 		if (url.endsWith(".jpeg") || url.endsWith(".jpg") || url.endsWith(".png")
-				|| url.endsWith(".pdf") || url.contains("#") || url.contains("?")
+				|| url.endsWith(".pdf") || url.contains("#") 
 				|| url.contains("mailto:") || url.startsWith("javascript:") || url.endsWith(".gif")
 				||url.endsWith(".xml")) {
 			return false;
@@ -89,7 +109,17 @@ public class Crawler implements Runnable {
 		return true;
 	}
 	
-	
+	private static String[] getArrayOfURLs() {
+		
+		String[] urls = {
+				"https://www.google.com/search?q=website+with+links",
+				"https://www.google.com/search?q=websites+with+words",
+				"https://www.google.com/search?q=search",
+				"https://www.oxfordlearnersdictionaries.com/us/wordlists/oxford3000-5000"
+		};
+		
+		return urls;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -110,13 +140,20 @@ public class Crawler implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		Random r = new Random();
+		
 		while(true) {
 			System.out.println("Crawling started with url " + lastCrawledUrl);
 			startCrawling(lastCrawledUrl, 0);
-			parseURLS();
+			if(getUrls().size() != 0)
+				parseURLS();
+			else {
+				int i = r.nextInt(webUrlsToCrawl.length);
+				startCrawling(webUrlsToCrawl[i], 0);
+			}
 			try {
 				System.out.println("Crawler Thread going to sleep....");
-				Thread.sleep(90000);
+				Thread.sleep(2000);
 				System.out.println("Crawler Thread woke up! Beginning to crawl...");
 				
 			} catch (InterruptedException e) {
@@ -124,6 +161,7 @@ public class Crawler implements Runnable {
 				e.printStackTrace();
 			};
 		}
+		
 	}
 
 }
